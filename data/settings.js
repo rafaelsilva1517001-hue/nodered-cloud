a68f5345221f59125b93bd3c61af38c1921a3326
rafaelsilva1517001-hue <rafael.silva1517001@gmail.com>
2025-10-07 14:57:59 -0400

    Update settings.js

--- data/settings.js
+++ data/settings.js
@@ -4,11 +4,11 @@ module.exports = {
         type: "credentials",
         users: [{
             username: "Drezry",
-            password: "$2b$08$ju.N5Z5eYCF0yY.TO1A2qeEP4qmtYeP2jRPRciwVy6yqAwWk3jA8G", // senha: Junk@1517
+            password: "$2b$08$1a5V9ZpN7BtElW4tWKoZqetqZB4nZQz7Kaoq0Rrb8Cmf.Ih5Sr58a", // senha: Junk@1517
             permissions: "*"
         }]
     },
-    httpNodeAuth: {user: "Drezry", pass: "$2b$08$ju.N5Z5eYCF0yY.TO1A2qeEP4qmtYeP2jRPRciwVy6yqAwWk3jA8G"},
-    httpStaticAuth: {user: "Drezry", pass: "$2b$08$ju.N5Z5eYCF0yY.TO1A2qeEP4qmtYeP2jRPRciwVy6yqAwWk3jA8G"},
+    httpNodeAuth: {user: "Drezry", pass: "$2b$08$1a5V9ZpN7BtElW4tWKoZqetqZB4nZQz7Kaoq0Rrb8Cmf.Ih5Sr58a"},
+    httpStaticAuth: {user: "Drezry", pass: "$2b$08$1a5V9ZpN7BtElW4tWKoZqetqZB4nZQz7Kaoq0Rrb8Cmf.Ih5Sr58a"},
     uiPort: process.env.PORT || 1880
 }
